20733a5a6c790e6e52b69f52bae836ce6fcfd665
Michael_C_Stone <mikestone2002@gmail.com>
2024-10-04 20:46:14 +0200

    Composite pattern for component 1

---
+++ HouseSection.h
@@ -0,0 +1,21 @@
+#ifndef HOUSESECTION_H
+#define HOUSESECTION_H
+
+#include "Room.h"
+
+#include <string>
+#include <vector>
+
+class HouseSection 
+{
+    private:
+        std::string name;
+        std::vector<Room> rooms;
+        
+    public:
+        HouseSection(const std::string& name);
+        void addRoom(const Room& room);
+        void performActionOnAllRooms(const std::string& action);
+};
+
+#endif
---
+++ Room.h
@@ -0,0 +1,22 @@
+#ifndef ROOM_H
+#define ROOM_H
+
+#include "SmartDevice.h"
+#include <vector>
+#include <string>
+
+class Room : public SmartDevice 
+{
+    private:
+        std::string name;
+        std::vector<SmartDevice*> smartDevices;
+
+    public:
+        Room(const std::string& name);
+        void addDevice(SmartDevice* device);
+        std::string getStatus() const override;
+        void performAction(const std::string& action) override;
+        std::string getSmartDeviceType() const override;
+};
+
+#endif
